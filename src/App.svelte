18e04d33b0dec8ec8aa5789c73777bfb18acc82f
sanlee423 <slee204@its.jnj.com>
2022-02-10 11:07:16 -0500

    udpates

--- src/App.svelte
+++ src/App.svelte
@@ -3,10 +3,30 @@
 </script>
 
 <main>
-  <h1>Hello!</h1>
-  <p>
-    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
-    how to build Svelte apps.
-  </p>
-  <Button hasIcon={false} text="UPLOAD" />
+  <div class="canvas">
+    <h1 class="header">Digital Solutions -Design Repository</h1>
+    <p>These components have been imported from the src folder</p>
+    <div class="component-container">
+      <Button primary={true} hasIcon={true} text="UPLOAD" />
+      <Button hasIcon={true} text="UPLOAD" />
+    </div>
+    <div class="component-container">
+      <Button primary={false} hasIcon={false} text="UPLOAD" />
+      <Button primary={true} hasIcon={false} text="UPLOAD" />
+    </div>
+  </div>
 </main>
+
+<style>
+  .canvas {
+    @apply m-12 flex-1 justify-center;
+  }
+
+  .header {
+    @apply font-normal text-2xl tracking-wide;
+  }
+
+  .component-container {
+    @apply m-12;
+  }
+</style>
